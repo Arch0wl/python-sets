9f7c16d88f99e24520a4a9d7adb293212c3a82d7
Arch0wl <annstarovoitov@gmail.com>
2022-08-25 10:36:18 -0400

    first commit

---
+++ index.py
@@ -0,0 +1,13 @@
+#Sets - blazingly fast unordered Lists 
+friends = ['John','Michael','Terry','Eric','Graham']
+friends_tuple = ('John','Michael','Terry','Eric','Graham')
+friends_set = {'John','Michael','Terry','Eric','Graham','Eric'}
+
+my_friends_set = {'Reg','Tery','hanna','Neil','Eric'}
+# print(friends)
+# print(friends_tuple)
+# print(friends_set)
+
+# cheking , remeber no space
+
+print(friends_set.intersection(my_friends_set))
